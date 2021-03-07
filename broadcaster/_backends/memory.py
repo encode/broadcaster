9b18d6bc08b4199f3bbe4751b65b218109df256b
Charlie DiGiovanna <cd17822@gmail.com>
2021-03-07 12:30:26 -0500

    Fixed MemoryBackend & PostgresBackend queue initialization

--- broadcaster/_backends/memory.py
+++ broadcaster/_backends/memory.py
@@ -7,10 +7,9 @@ from .._base import Event
 class MemoryBackend(BroadcastBackend):
     def __init__(self, url: str):
         self._subscribed: typing.Set = set()
-        self._published: asyncio.Queue = asyncio.Queue()
 
     async def connect(self) -> None:
-        pass
+        self._published: asyncio.Queue = asyncio.Queue()
 
     async def disconnect(self) -> None:
         pass
--- broadcaster/_backends/postgres.py
+++ broadcaster/_backends/postgres.py
@@ -7,10 +7,10 @@ from .._base import Event
 class PostgresBackend(BroadcastBackend):
     def __init__(self, url: str):
         self._url = url
-        self._listen_queue: asyncio.Queue = asyncio.Queue()
 
     async def connect(self) -> None:
         self._conn = await asyncpg.connect(self._url)
+        self._listen_queue: asyncio.Queue = asyncio.Queue()
 
     async def disconnect(self) -> None:
         await self._conn.close()
