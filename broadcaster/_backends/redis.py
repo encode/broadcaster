0a05f3436088924db8bec97b1384df38d123f488
vessaldaneshvar <52780555+vessaldaneshvar@users.noreply.github.com>
2021-10-12 20:39:18 +0330

    add db and password for redis backend connection

--- broadcaster/_backends/redis.py
+++ broadcaster/_backends/redis.py
@@ -12,10 +12,13 @@ class RedisBackend(BroadcastBackend):
         parsed_url = urlparse(url)
         self._host = parsed_url.hostname or "localhost"
         self._port = parsed_url.port or 6379
+        self.parse_query_db_password(parsed_url.query)
 
     async def connect(self) -> None:
-        self._pub_conn = await asyncio_redis.Connection.create(self._host, self._port)
-        self._sub_conn = await asyncio_redis.Connection.create(self._host, self._port)
+        self._pub_conn = await asyncio_redis.Connection.create(
+            host=self._host, port=self._port, db=self._db, password=self._password)
+        self._sub_conn = await asyncio_redis.Connection.create(
+            host=self._host, port=self._port, db=self._db, password=self._password)
         self._subscriber = await self._sub_conn.start_subscribe()
 
     async def disconnect(self) -> None:
@@ -34,3 +37,20 @@ class RedisBackend(BroadcastBackend):
     async def next_published(self) -> Event:
         message = await self._subscriber.next_published()
         return Event(channel=message.channel, message=message.value)
+
+    def parse_query_db_password(self, query: str) -> None:
+        db = None
+        password = None
+        for query_param in query.split('&'):
+            if query_param.count("=") != 1:
+                continue
+            query_key, query_value = query_param.split("=")
+            if query_key == "password":
+                password = query_value
+            elif query_key == "db":
+                try:
+                    db = int(query_value)
+                except ValueError:
+                    db = 0
+        self._db = db or 0
+        self._password = password
