74ad0e77f716c283ff9d65537383a8c480324f8f
tsotne <tsotnesharvadze@gmail.com>
2023-10-24 18:17:32 +0400

    Added Redis Stream backend

--- broadcaster/_backends/redis.py
+++ broadcaster/_backends/redis.py
@@ -1,7 +1,9 @@
+import asyncio
 import typing
 from urllib.parse import urlparse
 
 import asyncio_redis
+import aioredis
 
 from .._base import Event
 from .base import BroadcastBackend
@@ -36,3 +38,49 @@ class RedisBackend(BroadcastBackend):
     async def next_published(self) -> Event:
         message = await self._subscriber.next_published()
         return Event(channel=message.channel, message=message.value)
+
+
+class RedisStreamBackend(BroadcastBackend):
+    def __init__(self, url: str):
+        self.conn_url = url.replace('redis-stream', 'redis', 1)
+        self.streams: typing.Dict = dict()
+
+    async def connect(self) -> None:
+        self._producer = await aioredis.from_url(self.conn_url)
+        self._consumer = await aioredis.from_url(self.conn_url)
+
+    async def disconnect(self) -> None:
+        await self._producer.close()
+        await self._consumer.close()
+
+    async def subscribe(self, channel: str) -> None:
+        try:
+            info = await self._consumer.xinfo_stream(channel)
+            last_id = info['last-generated-id']
+        except aioredis.exceptions.ResponseError:
+            last_id = '0'
+        self.streams[channel] = last_id
+
+    async def unsubscribe(self, channel: str) -> None:
+        self.streams.pop(channel, None)
+
+    async def publish(self, channel: str, message: typing.Any) -> None:
+        await self._producer.xadd(channel, {'message': message})
+
+    async def wait_for_messages(self) -> typing.List:
+        messages = None
+        while not messages:
+            while not self.streams:
+                await asyncio.sleep(1)
+            messages = await self._consumer.xread(self.streams, count=1, block=1000)
+        return messages
+
+    async def next_published(self) -> Event:
+        messages = await self.wait_for_messages()
+        stream, events = messages[0]
+        _msg_id, message = events[0]
+        self.streams[stream.decode('utf-8')] = _msg_id.decode('utf-8')
+        return Event(
+            channel=stream.decode('utf-8'),
+            message=message.get(b'message').decode('utf-8'),
+        )
--- broadcaster/_base.py
+++ broadcaster/_base.py
@@ -36,6 +36,11 @@ class Broadcast:
 
             self._backend = RedisBackend(url)
 
+        elif parsed_url.scheme == "redis-stream":
+            from broadcaster._backends.redis import RedisStreamBackend
+
+            self._backend = RedisStreamBackend(url)
+
         elif parsed_url.scheme in ("postgres", "postgresql"):
             from broadcaster._backends.postgres import PostgresBackend
 
