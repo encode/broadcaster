722aaa1a4800aacd3280262d7d6cff70fc3fbe1d
tsotne <tsotnesharvadze@gmail.com>
2023-10-25 11:43:05 +0400

    Added Test For Redis Stream Backend

--- tests/test_broadcast.py
+++ tests/test_broadcast.py
@@ -56,6 +56,21 @@ async def test_redis():
 
 
 @pytest.mark.asyncio
+async def test_redis_stream():
+    async with Broadcast("redis-stream://localhost:6379") as broadcast:
+        async with broadcast.subscribe("chatroom") as subscriber:
+            await broadcast.publish("chatroom", "hello")
+            event = await subscriber.get()
+            assert event.channel == "chatroom"
+            assert event.message == "hello"
+        async with broadcast.subscribe("chatroom1") as subscriber:
+            await broadcast.publish("chatroom1", "hello")
+            event = await subscriber.get()
+            assert event.channel == "chatroom1"
+            assert event.message == "hello"
+
+
+@pytest.mark.asyncio
 async def test_postgres():
     async with Broadcast("postgres://postgres:postgres@localhost:5432/broadcaster") as broadcast:
         async with broadcast.subscribe("chatroom") as subscriber:
