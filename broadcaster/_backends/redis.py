3683160dfb8842b1858a378a65100f2bf8aa5fda
tsotne <tsotnesharvadze@gmail.com>
2023-10-25 09:53:13 +0400

    Fixed Linting Test

--- broadcaster/_backends/redis.py
+++ broadcaster/_backends/redis.py
@@ -42,7 +42,7 @@ class RedisBackend(BroadcastBackend):
 
 class RedisStreamBackend(BroadcastBackend):
     def __init__(self, url: str):
-        self.conn_url = url.replace('redis-stream', 'redis', 1)
+        self.conn_url = url.replace("redis-stream", "redis", 1)
         self.streams: typing.Dict = dict()
 
     async def connect(self) -> None:
@@ -56,16 +56,16 @@ class RedisStreamBackend(BroadcastBackend):
     async def subscribe(self, channel: str) -> None:
         try:
             info = await self._consumer.xinfo_stream(channel)
-            last_id = info['last-generated-id']
+            last_id = info["last-generated-id"]
         except aioredis.exceptions.ResponseError:
-            last_id = '0'
+            last_id = "0"
         self.streams[channel] = last_id
 
     async def unsubscribe(self, channel: str) -> None:
         self.streams.pop(channel, None)
 
     async def publish(self, channel: str, message: typing.Any) -> None:
-        await self._producer.xadd(channel, {'message': message})
+        await self._producer.xadd(channel, {"message": message})
 
     async def wait_for_messages(self) -> typing.List:
         messages = None
@@ -79,8 +79,8 @@ class RedisStreamBackend(BroadcastBackend):
         messages = await self.wait_for_messages()
         stream, events = messages[0]
         _msg_id, message = events[0]
-        self.streams[stream.decode('utf-8')] = _msg_id.decode('utf-8')
+        self.streams[stream.decode("utf-8")] = _msg_id.decode("utf-8")
         return Event(
-            channel=stream.decode('utf-8'),
-            message=message.get(b'message').decode('utf-8'),
+            channel=stream.decode("utf-8"),
+            message=message.get(b"message").decode("utf-8"),
         )
