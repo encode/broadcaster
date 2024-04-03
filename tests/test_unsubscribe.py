1694033e41d0f34dd1f2ea4dcd8877fbe352c667
alex.oleshkevich <alex.oleshkevich@gmail.com>
2024-04-03 21:20:00 +0200

    activate broadcaster via context manager

--- tests/test_unsubscribe.py
+++ tests/test_unsubscribe.py
@@ -5,25 +5,21 @@ from broadcaster import Broadcast
 @pytest.mark.asyncio
 async def test_unsubscribe():
     """The queue should be removed when the context manager is left."""
-    broadcast = Broadcast("memory://")
-    await broadcast.connect()
+    async with Broadcast("memory://") as broadcast:
+        async with broadcast.subscribe("chatroom"):
+            pass
 
-    async with broadcast.subscribe("chatroom"):
-        pass
-
-    assert "chatroom" not in broadcast._subscribers
+        assert "chatroom" not in broadcast._subscribers
 
 
 @pytest.mark.asyncio
 async def test_unsubscribe_w_exception():
     """In case an exception is raised inside the context manager, the queue should be removed."""
-    broadcast = Broadcast("memory://")
-    await broadcast.connect()
+    async with Broadcast("memory://") as broadcast:
+        try:
+            async with broadcast.subscribe("chatroom"):
+                raise RuntimeError("MyException")
+        except RuntimeError:
+            pass
 
-    try:
-        async with broadcast.subscribe("chatroom"):
-            raise RuntimeError("MyException")
-    except RuntimeError:
-        pass
-
-    assert "chatroom" not in broadcast._subscribers
+        assert "chatroom" not in broadcast._subscribers
