2bf24df5b567401b03bf1f8d0367d8f5b63c43de
alex.oleshkevich <alex.oleshkevich@gmail.com>
2024-04-04 10:57:33 +0200

    use custom backend in tests

--- tests/test_broadcast.py
+++ tests/test_broadcast.py
@@ -1,7 +1,35 @@
 import pytest
+import typing
+import asyncio
 
-from broadcaster import Broadcast
-from broadcaster._backends.memory import MemoryBackend
+from broadcaster import Broadcast, BroadcastBackend, Event
+
+
+class CustomBackend(BroadcastBackend):
+    def __init__(self, url: str):
+        self._subscribed: typing.Set = set()
+
+    async def connect(self) -> None:
+        self._published: asyncio.Queue = asyncio.Queue()
+
+    async def disconnect(self) -> None:
+        pass
+
+    async def subscribe(self, channel: str) -> None:
+        self._subscribed.add(channel)
+
+    async def unsubscribe(self, channel: str) -> None:
+        self._subscribed.remove(channel)
+
+    async def publish(self, channel: str, message: typing.Any) -> None:
+        event = Event(channel=channel, message=message)
+        await self._published.put(event)
+
+    async def next_published(self) -> Event:
+        while True:
+            event = await self._published.get()
+            if event.channel in self._subscribed:
+                return event
 
 
 @pytest.mark.asyncio
@@ -49,7 +77,7 @@ async def test_kafka():
 
 @pytest.mark.asyncio
 async def test_custom():
-    backend = MemoryBackend("")
+    backend = CustomBackend("")
     async with Broadcast(backend=backend) as broadcast:
         async with broadcast.subscribe("chatroom") as subscriber:
             await broadcast.publish("chatroom", "hello")
