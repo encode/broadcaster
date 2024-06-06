512560f8de1238636e1c047dbf841f7a5b61a9ff
alex.oleshkevich <alex.oleshkevich@gmail.com>
2024-06-06 10:48:33 +0200

    kafka improvements

--- broadcaster/_backends/kafka.py
+++ broadcaster/_backends/kafka.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import asyncio
 import typing
 from urllib.parse import urlparse
 
@@ -10,9 +11,11 @@ from .base import BroadcastBackend
 
 
 class KafkaBackend(BroadcastBackend):
-    def __init__(self, url: str):
-        self._servers = [urlparse(url).netloc]
+    def __init__(self, urls: str | list[str]) -> None:
+        urls = [urls] if isinstance(urls, str) else urls
+        self._servers = [urlparse(url).netloc for url in urls]
         self._consumer_channels: set[str] = set()
+        self._ready = asyncio.Event()
 
     async def connect(self) -> None:
         self._producer = AIOKafkaProducer(bootstrap_servers=self._servers)
@@ -27,6 +30,7 @@ class KafkaBackend(BroadcastBackend):
     async def subscribe(self, channel: str) -> None:
         self._consumer_channels.add(channel)
         self._consumer.subscribe(topics=self._consumer_channels)
+        await self._wait_for_assignment()
 
     async def unsubscribe(self, channel: str) -> None:
         self._consumer.unsubscribe()
@@ -35,5 +39,13 @@ class KafkaBackend(BroadcastBackend):
         await self._producer.send_and_wait(channel, message.encode("utf8"))
 
     async def next_published(self) -> Event:
+        await self._ready.wait()
         message = await self._consumer.getone()
         return Event(channel=message.topic, message=message.value.decode("utf8"))
+
+    async def _wait_for_assignment(self) -> None:
+        """Wait for the consumer to be assigned to the partition."""
+        while not self._consumer.assignment():
+            await asyncio.sleep(0.001)
+
+        self._ready.set()
