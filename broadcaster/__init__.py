d6806ace2478daa5a4caaf78fa2487c0f55740ee
Alex Oleshkevich <alex.oleshkevich@gmail.com>
2024-06-27 20:08:33 +0200

    Bump version (#131)

--- broadcaster/__init__.py
+++ broadcaster/__init__.py
@@ -1,5 +1,5 @@
 from ._backends.base import BroadcastBackend
 from ._base import Broadcast, Event
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = ["Broadcast", "Event", "BroadcastBackend"]
